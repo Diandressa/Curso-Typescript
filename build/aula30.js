2a6e53274594850a366a3d0c74f208a6da46e368
Andressa <andressa_nicolau@hotmail.com>
2025-03-18 14:09:38 -0300

    modules part2
    export default

--- build/aula30.js
+++ build/aula30.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const classe_aula30_1 = require("./classe-aula30");
-const p1 = new classe_aula30_1.Pessoa("Bruno", 1.65);
-const o1 = new classe_aula30_1.Objeto("Mesa");
+const classe_aula30e31_1 = require("./classe-aula30e31");
+const p1 = new classe_aula30e31_1.Pessoa("Bruno", 1.65);
+const o1 = new classe_aula30e31_1.Objeto("Mesa");
 console.log(p1.nome);
 console.log(p1.altura);
---
+++ build/aula31.js
@@ -0,0 +1,12 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const classe_aula30e31_1 = __importDefault(require("./classe-aula30e31"));
+const classe_aula30e31_2 = require("./classe-aula30e31");
+const p1 = new classe_aula30e31_1.default("Bruno", 1.65);
+const o1 = new classe_aula30e31_2.Objeto("Mesa");
+console.log(p1.nome);
+console.log(p1.altura);
+console.log(classe_aula30e31_2.Coisas);
