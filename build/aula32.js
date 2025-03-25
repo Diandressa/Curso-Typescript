d91de2ab112e46a4dace63b161acc46ebd50709f
Andressa <andressa_nicolau@hotmail.com>
2025-03-25 16:31:47 -0300

    lesson 32
    namespace part1

---
+++ build/aula32.js
@@ -0,0 +1,32 @@
+"use strict";
+var Veiculos;
+(function (Veiculos) {
+    class Carro {
+        nome;
+        motor;
+        constructor(nome) {
+            this.nome = nome;
+            this.motor = new Motores.Motor(100);
+        }
+    }
+    Veiculos.Carro = Carro;
+})(Veiculos || (Veiculos = {}));
+var Motores;
+(function (Motores) {
+    class Turbo {
+        potencia;
+        constructor(potencia) {
+            this.potencia = potencia;
+        }
+    }
+    class Motor {
+        potencia;
+        constructor(potencia) {
+            this.potencia = potencia;
+        }
+    }
+    Motores.Motor = Motor;
+})(Motores || (Motores = {}));
+const carro1 = new Veiculos.Carro("Corola");
+console.log(carro1.nome);
+console.log(carro1.motor);
